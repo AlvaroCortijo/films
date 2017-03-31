3cf372cb583f0e468f57023135f4717e1b087dcc
Alvaro <Alvaro@LAPTOP-9BCMHOTG>
2017-03-31 11:49:52 +0200

    Autowired annotation added.

--- src/main/java/com/dbg/service/user/UserServiceImpl.java
+++ src/main/java/com/dbg/service/user/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dbg.service.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dbg.dao.user.UserDao;
@@ -9,7 +10,9 @@ import com.dbg.model.user.User;
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
 	private UserDao userDao;
+
 	@Override
 	public UserDTO create(UserDTO userDTO) {
 		final User user = transform(userDTO);
