b2f0bf6d9b9c7794b8b428a641c0ed72a40a1d1f
Alvaro <Alvaro@LAPTOP-9BCMHOTG>
2017-04-02 11:07:32 +0200

    create method in evaluation service fixed

--- src/main/java/com/dbg/service/evaluation/EvaluationServiceImpl.java
+++ src/main/java/com/dbg/service/evaluation/EvaluationServiceImpl.java
@@ -9,14 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dbg.dao.evaluation.EvaluationDao;
+import com.dbg.dao.film.FilmDao;
+import com.dbg.dao.user.UserDao;
 import com.dbg.dto.evaluation.EvaluationDTO;
 import com.dbg.model.evaluation.Evaluation;
+import com.dbg.model.film.Film;
+import com.dbg.model.user.User;
 
 @Service
 public class EvaluationServiceImpl implements EvaluationService {
 
 	@Autowired
 	private EvaluationDao evaluationDao;
+	
+	@Autowired
+	private FilmDao filmDao;
+	
+	@Autowired
+	private UserDao userDao;
 	
 	private static final Logger log = LoggerFactory.getLogger(EvaluationServiceImpl.class);
 
@@ -66,6 +76,7 @@ public class EvaluationServiceImpl implements EvaluationService {
 			evaluationDTO.setName(evaluation.getUser().getName());
 		if(evaluation.getFilm() != null)
 			evaluationDTO.setTitle(evaluation.getFilm().getTitle());
+
 		return evaluationDTO;
 	}
 
@@ -74,6 +85,10 @@ public class EvaluationServiceImpl implements EvaluationService {
 		log.debug(String.format("transform - evaluationDTO con points %d",evaluationDTO.getPoints()));
 		final Evaluation evaluation = new Evaluation();
 		evaluation.setPoints(evaluationDTO.getPoints());
+		final Film film = filmDao.findByTitle(evaluationDTO.getTitle());
+		evaluation.setFilm(film);
+		final User user = userDao.findByName(evaluationDTO.getName());
+		evaluation.setUser(user);
 		log.debug(String.format("transform - evaluation con points %d",evaluation.getPoints()));
 		return evaluation;
 	}
